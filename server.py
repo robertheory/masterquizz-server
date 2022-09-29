f3ca84ae012e50f18a8186586bac8cc56a599074
Roberto Costa <roberto.araujo339@gmail.com>
2022-09-29 19:11:11 -0300

    Game and Round classes, round lifecycle started

--- server.py
+++ server.py
@@ -9,8 +9,6 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, logger=False, engineio_logger=False,
                     cors_allowed_origins="*")
 
-clientList = []
-
 questions = [
     {
         "index": 0,
@@ -25,7 +23,7 @@ questions = [
         }
     },
     {
-        "index": 0,
+        "index": 1,
         "question": "Quanto é 1 + 1?",
         "answer": "a",
         "options": {
@@ -53,62 +51,115 @@ class Round:
         })
 
 
-currentRound = Round(questions[0]['index'])
-finishedRounds = []
+class Game:
+    def __init__(self):
+        self.currentRound = None
+        self.finishedRounds = []
+        self.finished = False
+        self.users = []
 
+    def user_exists(self, userId):
+        for i in range(len(self.users)):
+            if self.users[i]['id'] == userId:
+                return True
+        return False
 
-def searchClient(client):
-    for i in range(len(clientList)):
-        if clientList[i]['id'] == client['id']:
+    def user_already_answered(self, id):
+        for i in range(len(self.currentRound.answers)):
+            if self.currentRound.answers[i]['user'] == id:
+                return True
+        return False
+
+    def all_users_answered(self):
+        answer_user_ids = map(
+            lambda answer: answer['user'], self.currentRound.answers)
+
+        not_answered_users = filter(
+            lambda user: user['id'] not in answer_user_ids, self.users)
+
+        if (len(list(not_answered_users)) == 0):
             return True
-    return False
+        else:
+            return False
+
+    def show_round_results(self):
+        self.currentRound.open = False
+        response = {
+            "round": game.currentRound.question,
+            "question": questions[game.currentRound.question]['question'],
+            "correctAnswer": questions[game.currentRound.question]['answer'],
+            "answers": game.currentRound.answers
+        }
+        print(response)
+        emit('round-result', response, broadcast=True)
+
+    def new_answer(self, data):
+        if (not self.user_already_answered(data['user'])):
+            self.currentRound.add_answer(
+                user=data['user'], name=data['name'], answer=data['answer'])
+            if (self.all_users_answered()):
+                print("ALL USERS ANSWERED")
+                self.show_round_results()
+
+    def register_user(self, user):
+        exits = self.user_exists(userId=user["id"])
+        if (not exits):
+            self.users.append(user)
+            if (self.currentRound.open):
+                response = {
+                    "question": questions[game.currentRound.question]['question'],
+                    "options": questions[game.currentRound.question]['options']
+                }
+                print('** JOIN ROUND **')
+                emit('begin-round', response)
+
+    def finish_game(self):
+        self.finished = True
+        self.currentRound = None
+
+    def next_round(self):
+        print(self.currentRound)
+        if (self.currentRound == None):
+            newQuestion = 0
+        else:
+            newQuestion = self.currentRound.question + 1
+            self.finishedRounds.append(self.currentRound)
+
+        if (len(questions) > newQuestion):
+            self.currentRound = Round(questions[newQuestion]['index'])
+
+            print(self.currentRound)
+            response = {
+                "question": questions[self.currentRound.question]['question'],
+                "options": questions[self.currentRound.question]['options']
+            }
+            emit('begin-round', response, broadcast=True)
+        else:
+            self.finish_game()
 
 
-@socketio.on('message')
-def handle_message(data):
-    print('received message: ' + data)
+game = Game()
 
 
 @socketio.on('connect')
 def on_join(auth):
     newClient = auth['user']
     print('** NEW CONNECTION **')
-    print(newClient)
-    if (searchClient(newClient)):
-        print('** CLIENT ALREADY EXISTS **')
-    else:
-        clientList.append(newClient)
-    emit('client-list', clientList, broadcast=True)
+    game.register_user(newClient)
+    emit('client-list', game.users, broadcast=True)
 
 
 @socketio.on('start-game')
 def handle_start_game():
     print('** NEW GAME **')
-    currentRound = Round(questions[0]['index'])
-    response = {
-        "question": questions[currentRound.question]['question'],
-        "options": questions[currentRound.question]['options']
-    }
-    emit('begin-round', response, broadcast=True)
+    game.next_round()
 
 
 @socketio.on('answer')
 def handle_answer(data):
     print('** NEW ANSWER **')
     print(data)
-    currentRound.add_answer(
-        user=data['user'], name=data['name'], answer=data['answer'])
-
-    if (len(currentRound.answers) == len(clientList)):
-        currentRound.open = False
-        response = {
-            "round": currentRound.question,
-            "question": questions[currentRound.question]['question'],
-            "correctAnswer": questions[currentRound.question]['answer'],
-            "answers": currentRound.answers
-        }
-        print(response)
-        emit('round-result', response, broadcast=True)
+    game.new_answer(data)
 
 
 @socketio.on('disconnect')
