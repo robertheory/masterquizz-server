d8147e2cac2774f01e7bbe08fc8350c5615fd8e5
Roberto Costa <roberto.araujo339@gmail.com>
2022-09-29 23:37:12 -0300

    CORS added and ports configs changed

--- server.py
+++ server.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 import os
 
+
 app = Flask(__name__)
+CORS(app)
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, logger=False, engineio_logger=False,
